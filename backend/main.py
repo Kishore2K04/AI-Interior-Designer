522daf0ed8f3df52e20b68d7f2e14a25192f0e02
kishore2K04 <kish231004@gmail.com>
2026-07-06 12:03:03 +0530

    Sprint 2: Implement image upload feature

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"message": "Backend is running!"}
+
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Image uploaded successfully",
+        "filename": file.filename
+    }
